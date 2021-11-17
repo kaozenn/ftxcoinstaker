3954a29cf3085d0488e6e4057b5ebcdf35301e1e
kaozn <kaozn@kaozn.io>
2021-11-17 11:17:21 +1100

    Add subaccount feature

--- src/ftxcoinstaker/console.py
+++ src/ftxcoinstaker/console.py
@@ -2,15 +2,17 @@
 
 import os
 from . import client
+from pprint import pprint
 
 def main():
 
 	FTX_API_KEY=os.getenv('FTX_API_KEY')
 	FTX_API_SECRET=os.getenv('FTX_API_SECRET')
+	FTX_SUBACCOUNT_NAME=os.getenv('FTX_SUBACCOUNT_NAME')
 
-	ftx = client.FtxClient(FTX_API_KEY, FTX_API_SECRET)
+	ftx = client.FtxClient(api_key=FTX_API_KEY, api_secret=FTX_API_SECRET, subaccount_name=FTX_SUBACCOUNT_NAME)
 
-	response = ftx.create_subaccount("test")
+	#response = ftx.create_subaccount("test")
 
-	# balance = ftx.get_subaccounts()
-	# print(balance)
+	balance = ftx.get_balances()
+	pprint(balance)
